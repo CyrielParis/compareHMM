ba39400fc256c66d6a093ebcdf236f6167241112
Cyriel <cyriel.paris@inra.fr>
2020-03-18 14:35:37 +0100

    Backbone wiki

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
+# Presentation of the software
+
+This software was designed to compare different transition distributions
+
+# Quick user guide
+
+## Simulate a Wright-Fisher process under selection
+
+### Simulate the allele frequency evolution
+
+### Simulate the sampling
+
+### Export
+
+## Run an analyze on simulations
+
+## Run an analyze on real data set
+
+### Input format
+
+### Output format
+
+### Run the script
+
 # compareHMM
 comparator of models to detect selection in HMM framework
+
--- make_graphs.r
+++ make_graphs.r
@@ -16,7 +16,8 @@ df <- (fread('analyzed/fixing_from_high_ana.csv', sep = ',', header = TRUE))
 ggplot(df) + geom_line(aes(x = s, y = L, group = ID, color = as.factor(ID)))
 
 df <- (fread('analyzed/fixing_from_middle_ana.csv', sep = ',', header = TRUE))
-ggplot(df) + geom_line(aes(x = s, y = L, group = ID, color = as.factor(ID)))
+ggplot(df) + geom_line(aes(x = s, y = L, group = ID, color = as.factor(ID))) + coord_cartesian(ylim = c(-100, 0))
+ggsave('/home/cyriel/Documents/pro/these/presentations/presentation_soutenance/vraisemblance_monotone.png')
 
 df <- (fread('analyzed/fixing_from_low_ana.csv', sep = ',', header = TRUE))
 ggplot(df) + geom_line(aes(x = s, y = L, group = ID, color = as.factor(ID)))
